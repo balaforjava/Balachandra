758651a67b6f8db6f5e5b238f05bc2666070040e
balaforjava <pbcmmr1994@gmail.com>
2019-08-28 15:29:36 +0530

    java

--- src/com/app/controller/Diwaka.java
+++ src/com/app/controller/Diwaka.java
@@ -3,6 +3,7 @@ package com.app.controller;
 public class Diwaka {
 	public static void main(String[] args) {
 		
-		System.out.println();
+		System.out.println("hi diwakar");
+		System.out.println("hello java");
 	}
 }
